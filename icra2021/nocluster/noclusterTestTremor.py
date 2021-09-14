046aa1439df070b463b93d93ede4891745966049
Luke Bhan <luke.bhan@vanderbilt.edu>
2021-09-14 01:36:59 -0500

    add domain random

---
+++ icra2021/nocluster/noclusterTestTremor.py
@@ -0,0 +1,18 @@
+import gym, assistive_gym
+import pybullet as p
+import numpy as np
+import os
+from stable_baselines3 import PPO
+
+env = gym.make("ScratchItchJacoHuman-v1")
+model = PPO.load("model/model_400000_steps")
+success_arr = []
+for j in range(100):
+    print(j)
+    obs = env.reset()
+    for i in range(200):
+        act = model.predict([obs])[0]
+        obs, rew, done, info  = env.step(act[0])
+    print(info)
+    success_arr.append(info["human"]["task_sucess_value"])
+np.savetxt("tremor.dat", success_arr)
